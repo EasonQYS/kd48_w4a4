7c7bba151dd4574bbef7e9feda05e0b29cdb4fa1
EasonQYS <easonqys@foxmail.com>
2024-08-19 17:02:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from lmdeploy.serve.gradio.turbomind_coupled import run_local
 from lmdeploy.messages import TurbomindEngineConfig
 
+import os
 base_path = './model'
 os.system('git lfs install')
 os.system(f'git lfs clone https://code.openxlab.org.cn/yisheng/snh_pocket48_1.git {base_path}')
