a550ef6f279f5e98f923cad21c4ec6af6cef9951
EasonQYS <easonqys@foxmail.com>
2024-08-19 19:59:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from lmdeploy.serve.gradio.turbomind_coupled import run_local
-from lmdeploy.messages import TurbomindEngineConfig
-
 import os
 base_path = './model'
 os.system('git lfs install')
-os.system(f'git lfs clone https://code.openxlab.org.cn/yisheng/snh_pocket48_1.git {base_path}')
+os.system(f'git clone https://code.openxlab.org.cn/yisheng/snh_pocket48_4bit.git {base_path}')
 print('********git clone***********')
 os.system(f'cd {base_path} && git lfs pull')
 print('***************pull finish**********')
 
-backend_config = TurbomindEngineConfig(max_batch_size=8)
-model_path = base_path
-run_local(model_path, backend_config=backend_config, server_name="openxlab")
+os.system(f'lmdeploy serve gradio {base_path} --server-name 0.0.0.0 --server-port 7860 --model-format awq')
